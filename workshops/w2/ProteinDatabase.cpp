72bf6af8b4130743358e91fbcc10a0152dd30b64
sharonyouchunxue <cyou8@myseneca.ca>
2023-01-27 20:32:18 -0500

    submitted

---
+++ workshops/w2/ProteinDatabase.cpp
@@ -0,0 +1,135 @@
+//*
+//*****************************************************************************
+//                        Workshop 2
+//Full Name  : Chunxue You
+//Student ID#: 127632214
+//Email      : cyou8@myseneca.ca
+//Section    : OOP244 NRA
+//Date       : 2023-01-23
+//Authenticity Declaration:
+//I declare this submission is the result of my own work and has not been
+//shared with any other student or 3rd party content provider. I have done all
+//the coding by myself and only copied the code that my professor provided to
+//complete my workshops and assignments.
+//*****************************************************************************
+//*/
+//
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "ProteinDatabase.h"
+
+namespace sdds {
+
+   //default constructor
+   ProteinDatabase::ProteinDatabase(){
+   }
+   //one argument constructor
+   ProteinDatabase::ProteinDatabase(const char* filename) {
+       unsigned int counter = 0;
+      if (filename) {
+         //create instance ifstream
+         std::ifstream fin(filename);
+         std::string temp;
+         if (fin.is_open()) {
+            while (getline(fin, temp))
+               if (temp[0] == '>')
+                  ++counter; 
+         }
+         fin.close();
+         //Each line (except the last one) will contain exactly 60 characters.
+         m_noOfstrings = counter;
+         //allocates memory for that number of protein sequences in the array
+         m_string = new std::string[m_noOfstrings + 1];
+         //re-reads the file and loads the protein sequences (i.e., string of characters 
+         //not sequence names) into the array.
+         unsigned int i = 0;
+         fin.open(filename);
+         if (fin.is_open()) {
+            while (getline(fin, temp) && i < m_noOfstrings)
+            {
+               if (temp[0] != '>') {
+                  m_string[i] += temp;
+               }
+               else if (temp[0] == '>' && m_string[i] != "\0") {
+                  ++i;
+               }
+            }
+            fin.close();
+         }
+       }
+      }
+
+      //a query that returns the number of protein sequences stored in the current object
+      std::size_t ProteinDatabase::size()const{
+         return m_noOfstrings;
+      }
+
+      //a query that returns a copy of the protein sequence at the index received 
+      //as the function parameter. 
+      std::string ProteinDatabase::operator[](size_t src) const{
+         std::string copy;
+         //check if the index is invalid, this function, return an empty string.
+         if (src > size()-1 || src < 0 || m_noOfstrings == 0) {
+            copy = "";
+         }
+         else {
+            copy = m_string[src];
+         }
+         return copy;
+   }
+
+
+   //copy constructor
+	ProteinDatabase::ProteinDatabase(const ProteinDatabase& src) {
+		if (src.m_string != nullptr) {
+			m_string = new std::string[src.m_noOfstrings];
+			unsigned int i;
+			for (i = 0; i < src.m_noOfstrings; i++) {
+				m_string[i] = src.m_string[i];
+			}
+			m_noOfstrings = src.m_noOfstrings;
+	  }
+   }
+
+   //copy assignment operator
+   ProteinDatabase& ProteinDatabase::operator=(ProteinDatabase& src) {
+		if (this != &src) {
+			m_string = new std::string[src.m_noOfstrings];
+			unsigned int i;
+			for (i = 0; i < src.m_noOfstrings; i++) {
+				m_string[i] = src.m_string[i];
+			}
+			m_noOfstrings = src.m_noOfstrings;
+		}
+		return (*this);
+   }
+   //move copy constructor
+   ProteinDatabase::ProteinDatabase(ProteinDatabase&& src) noexcept {
+       *this = std::move(src);
+   }
+
+   //move assignment operator
+   ProteinDatabase& ProteinDatabase::operator=(ProteinDatabase&& src) noexcept{
+		if (this != &src) {
+			delete[] m_string;
+			m_string = src.m_string;
+			src.m_string = nullptr;
+			m_noOfstrings = src.m_noOfstrings;
+			src.m_noOfstrings = 0u;
+		}
+		return (*this);
+   }
+
+   //destructor
+   ProteinDatabase::~ProteinDatabase(){
+      delete[] m_string;
+   }
+
+}
+
+
+
+
+
