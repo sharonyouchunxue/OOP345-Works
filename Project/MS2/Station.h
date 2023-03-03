d40ff9bd6ac69f49c73e8a11cb2412d564be6b3f
sharonyouchunxue <cyou8@myseneca.ca>
2023-03-03 16:28:47 -0500

    working on it

---
+++ Project/MS2/Station.h
@@ -0,0 +1,41 @@
+/*
+*****************************************************************************
+                        Milestone 1
+Full Name  : Chunxue You
+Student ID#: 127632214
+Email      : cyou8@myseneca.ca
+Section    : OOP244 NRA
+Date       : 2023-03-03
+Authenticity Declaration:
+I declare this submission is the result of my own work and has not been
+shared with any other student or 3rd party content provider. I have done all
+the coding by myself and only copied the code that my professor provided to
+complete my workshops and assignments.
+*****************************************************************************
+*/
+#include <ostream>
+#include "Utilities.h"
+#ifndef SDDS_STATION_H
+#define SDDS_STATION_H
+namespace sdds {
+   class Station {
+      //Instance Variables
+      int m_id{ 0 };
+      std::string m_name{};
+      std::string m_description{};
+      size_t m_nextSerialNumber{ 0u };//the next serial number to be assigned to an item at this station 
+      size_t m_noOfCurr{ 0 };//the number of items currently in stock
+
+      //Class Variables
+      static size_t m_widthField;
+      static size_t id_generator;
+   public:
+      Station(const std::string& src);
+      const std::string& getItemName() const;
+      size_t getNextSerialNumber();
+      size_t getQuantity() const;
+      void updateQuantity();
+      void display(std::ostream& os, bool full) const;
+   };
+}
+#endif // !SDDS_STATION_H
---
+++ Project/MS2/Utilities.h
@@ -0,0 +1,34 @@
+/*
+*****************************************************************************
+                        Milestone 1
+Full Name  : Chunxue You
+Student ID#: 127632214
+Email      : cyou8@myseneca.ca
+Section    : OOP244 NRA
+Date       : 2023-03-03
+Authenticity Declaration:
+I declare this submission is the result of my own work and has not been
+shared with any other student or 3rd party content provider. I have done all
+the coding by myself and only copied the code that my professor provided to
+complete my workshops and assignments.
+*****************************************************************************
+*/
+#ifndef SDDS_UTILITIES_H
+#define SDDS_UTILITIES_H
+namespace sdds {
+   class Utilities {
+      // length of the token extracted;
+      size_t m_widthField{ 1 };
+      //separates the tokens in any given std::string object
+      static char m_delimiter;
+   public:
+      void setFieldWidth(size_t newWidth);
+      size_t getFieldWidth() const;
+      std::string extractToken(const std::string& str, size_t& next_pos, bool& more);
+
+      //Class Functions
+      static void setDelimiter(char newDelimiter);
+      static char getDelimiter();
+   };
+}
+#endif // !SDDS_UTILITIES_H
