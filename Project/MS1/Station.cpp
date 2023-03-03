43611cba3390500700e742a46f94aa4c1733fcf9
sharonyouchunxue <cyou8@myseneca.ca>
2023-03-03 16:12:33 -0500

    ms1 need to be fixed....

---
+++ Project/MS1/Station.cpp
@@ -0,0 +1,81 @@
+#include "Station.h"
+/*
+*****************************************************************************
+                        Milestone 1
+Full Name  : Chunxue You
+Student ID#: 127632214
+Email      : cyou8@myseneca.ca
+Section    : OOP244 NRA
+Date       : 2023-03-03
+Authenticity Declaration:
+I declare this submission is the result of my own work and has not been
+shared with any other student or 3rd party content provider. I have done all
+the coding by myself and only copied the code that my professor provided to
+complete my workshops and assignments.
+*****************************************************************************
+*/
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include "Station.h"
+
+
+namespace sdds {
+   size_t Station::m_widthField = 0u;
+   size_t Station::id_generator = 0u;
+
+   Station::Station(const std::string& src){
+      size_t next_pos = 0u;
+      bool more = true;
+      Utilities util;
+      m_id = ++Station::id_generator;
+      // extract name
+      m_name = util.extractToken(src, next_pos, more);
+      //extract starting serial number 
+      m_nextSerialNumber = std::stoi(util.extractToken(src, next_pos, more));
+      //extract quantity 
+      m_noOfCurr = std::stoi(util.extractToken(src, next_pos, more));
+      // extract description 
+      m_description = util.extractToken(src, next_pos, more);
+      //update m_widthField
+      m_widthField = std::max(m_widthField, util.getFieldWidth());
+   }
+
+   //returns the name of the current Station object
+   const std::string& Station::getItemName() const {
+      return m_name;
+   }
+
+   //returns the next serial number to be used on the assembly line and increments m_serialNumber
+   size_t Station::getNextSerialNumber() {
+      ++m_nextSerialNumber;
+      return m_nextSerialNumber - 1;
+   }
+
+   // returns the remaining quantity of items in the Station object
+   size_t Station::getQuantity() const {
+      return m_noOfCurr;
+   }
+
+   //subtracts 1 from the available quantity; should not drop below 0.
+   void Station::updateQuantity() {
+      if (m_noOfCurr > 0) {
+         --m_noOfCurr;
+      }
+   }
+
+   void Station::display(std::ostream& os, bool full) const {
+      os << std::right << std::setw(3) << std::setfill('0') << m_id << " | ";
+      os << std::left << std::setw(Station::m_widthField) << std::setfill(' ') << m_name
+         << " | " << std::right << std::setw(6) << std::setfill('0') << m_nextSerialNumber << " |";
+
+      if (full) {
+         os << std::right << std::setw(4) << std::setfill(' ') << m_noOfCurr << " | ";
+         os << std::left  << m_description << std::endl;
+      }
+      else
+         os << std::endl;
+   }
+
+}
+
---
+++ Project/MS1/Utilities.cpp
@@ -0,0 +1,61 @@
+/*
+*****************************************************************************
+                        Milestone 1
+Full Name  : Chunxue You
+Student ID#: 127632214
+Email      : cyou8@myseneca.ca
+Section    : OOP244 NRA
+Date       : 2023-03-03
+Authenticity Declaration:
+I declare this submission is the result of my own work and has not been
+shared with any other student or 3rd party content provider. I have done all
+the coding by myself and only copied the code that my professor provided to
+complete my workshops and assignments.
+*****************************************************************************
+*/
+#include <iostream>
+#include "Utilities.h"
+namespace sdds {
+
+   char Utilities::m_delimiter = ' ';
+
+   //sets the field width of the current object to the value of parameter newWidth
+   void Utilities::setFieldWidth(size_t newWidth){
+      m_widthField = newWidth;
+   }
+
+   //returns the field width of the current object
+   size_t Utilities::getFieldWidth() const{
+      return m_widthField;
+   }
+
+   std::string Utilities::extractToken(const std::string& str, size_t& next_pos, bool& more){
+      //extracts a token from string str
+       std::string token = "";
+        if (next_pos < str.length()) {
+            size_t delimiter_pos = str.find(m_delimiter, next_pos);
+            token = str.substr(next_pos, delimiter_pos - next_pos);
+            next_pos = delimiter_pos != std::string::npos ? delimiter_pos + 1 : str.length();
+            more = true;
+
+            if (token.length() > m_widthField) {
+                m_widthField = token.length();
+            }
+        } else {
+            more = false;
+        }
+        return token;
+      
+   }
+
+   //sets the delimiter for this class to the character received
+   void Utilities::setDelimiter(char newDelimiter){
+      m_delimiter = newDelimiter;
+   }
+
+   //returns the delimiter for this class.
+   char Utilities::getDelimiter(){
+      return m_delimiter;
+   }
+
+}
